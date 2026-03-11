bdfd7cb55c09fa3ce1f852c63b395bab1432d68e
Sultan-Alshehry <192121920+Sultan-Alshehry@users.noreply.github.com>
2026-03-11 13:27:19 +0200

    added basic auto formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,78 +9,120 @@ vim.o.lazyredraw = true
 vim.g.mapleader = " "
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown", "text", "gitcommit" },
-  callback = function()
-    vim.opt_local.spell = true
-  end,
+    pattern = { "markdown", "text", "gitcommit" },
+    callback = function()
+        vim.opt_local.spell = true
+    end,
 })
 
 vim.opt.spelllang = { "en_us" }
 
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>e', ':Oil<CR>')
-vim.keymap.set({'n', 'v', 'x'}, 'y', '"+y')
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>w", ":write<CR>")
+vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
+vim.keymap.set("n", "<leader>q", ":quit<CR>")
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
+vim.keymap.set({ "n", "v", "x" }, "y", '"+y')
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic" })
-vim.keymap.set("n", "<leader>dd", ':lua vim.lsp.buf.code_action()')
-
+vim.keymap.set("n", "<leader>dd", ":lua vim.lsp.buf.code_action()")
 
 vim.pack.add({
 
-	{ src = 'https://github.com/catppuccin/nvim' },
-	{ src = 'https://github.com/stevearc/oil.nvim' },
-	{ src = 'https://github.com/neovim/nvim-lspconfig' },
-  	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
-	-- Autocomplete
-	{ src = 'https://github.com/hrsh7th/nvim-cmp' },
-	{ src = 'https://github.com/hrsh7th/cmp-nvim-lsp' },
-	{ src = 'https://github.com/hrsh7th/cmp-buffer' },
-	{ src = 'https://github.com/hrsh7th/cmp-path' },
-	{ src = 'https://github.com/L3MON4D3/LuaSnip' },
-
+    { src = "https://github.com/catppuccin/nvim" },
+    { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    -- Autocomplete
+    { src = "https://github.com/hrsh7th/nvim-cmp" },
+    { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+    { src = "https://github.com/hrsh7th/cmp-buffer" },
+    { src = "https://github.com/hrsh7th/cmp-path" },
+    { src = "https://github.com/L3MON4D3/LuaSnip" },
 })
+
+require("oil").setup()
+
+vim.cmd.colorscheme("catppuccin")
+
 vim.cmd("packadd nvim-treesitter")
 require("nvim-treesitter.config").setup({
-  highlight = { enable = true },
+    highlight = { enable = true },
 })
 
-
-require "oil".setup()
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-vim.lsp.config("*", {
-	capabilities = capabilities,
-})
 vim.lsp.enable({
-	"lua_ls",
-	"pylsp",
-	"bashls",
-	"clangd",
-    "jdtls"
+    "lua_ls",
+    "pylsp",
+    "bashls",
+    "clangd",
+    "jdtls",
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then return end
+
+        -- Auto-format ("lint") on save.
+        -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+        if client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                end,
+            })
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = {
+        "c",
+        "cpp",
+        "css",
+        "html",
+        "javascript",
+        "java",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "sh",
+    },
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end,
 })
 
 local cmp = require("cmp")
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = cmp.mapping.select_next_item(),
-		["<S-Tab>"] = cmp.mapping.select_prev_item(),
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "buffer" },
-		{ name = "path" },
-	}),
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+    }),
 })
 
-
-vim.cmd.colorscheme("catppuccin")
+vim.lsp.config("pylsp.setup", {
+    settings = {
+        pylsp = {
+            plugins = {
+                black = { enabled = true },
+                flake8 = { enabled = true },
+            }
+        }
+    }
+})
